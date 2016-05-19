65c4286f4e776a2bfb9be6642bf802e9ff492719
leslieo23 <leslie.obour23@gmail.com>
2016-05-19 19:27:45 -0400

    commit

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -7,8 +7,20 @@ module.exports=function(app){
 	app.get('/deeds',function(req,res){
 		res.sendFile(path.join(__dirname+'/../public/deeds.html'))
 	});
+	app.get('/deeds.html',function(req,res){
+		res.sendFile(path.join(__dirname+'/../public/deeds.html'))
+	});
 	app.get('/profile',function(req,res){
 		res.sendFile(path.join(__dirname+ '/../public/profile.html'))
+	})
+	app.get('/profile.html',function(req,res){
+		res.sendFile(path.join(__dirname+ '/../public/profile.html'))
+	})
+	app.get('/leaderboards',function(req,res){
+		res.sendFile(path.join(__dirname+ '/../public/leader.html'))
+	})
+	app.get('/leader.html',function(req,res){
+		res.sendFile(path.join(__dirname+ '/../public/leader.html'))
 	})
 	app.use(function(req,res){
 		res.sendFile(path.join(__dirname+'/../public/index.html'))
